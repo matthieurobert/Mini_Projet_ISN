7b21df68d6103cff0d0644bb3c4493ef572e6ece
Matthieu ROBERT <matthieurobert82@gmail.com>
2018-12-19 20:35:53 +0100

    transfert du drive vers git

---
+++ Casino_BlackJack.py
@@ -0,0 +1,30 @@
+                    # casino blacjack #
+
+from random import randrange
+import time
+import os
+import sys
+import tiragecarte
+from tiragecarte import tirage
+
+tirage()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
