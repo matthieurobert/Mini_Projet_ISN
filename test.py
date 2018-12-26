52ee1fe24c68b38c38ddc28ebc1ac01fb2b50a26
Matthieu Robert <matthieurobert82@gmail.com>
2018-12-26 16:21:40 +0100

    ajout du background

--- test.py
+++ test.py
@@ -3,9 +3,16 @@ from pygame.locals import *
 
 pygame.init()
 
-fenetre = pygame.display.set_mode((1280,720),RESIZABLE)
+fenetre = pygame.display.set_mode((1280,720), FULLSCREEN)
+table = pygame.image.load("blackjack_table.png").convert()
+fenetre.blit(table, (0,0))
+
+perso = pygame.image.load("card_b_h9.png").convert()
+fenetre.blit(perso, (485,435))
+
+pygame.display.flip()
 
 continuer = 1
 
 while continuer:
-	continue
+	continuer = int(input())
