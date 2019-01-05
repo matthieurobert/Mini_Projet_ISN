4fd755536ae1497d7e7a2bc7fa9abf6a5e2a0456
Matthieu Robert <matthieurobert82@gmail.com>
2019-01-05 22:29:14 +0100

    decalage de la carte

--- main_fenetre.py
+++ main_fenetre.py
@@ -32,7 +32,7 @@ def main():
 				if event.button == 1 and 650<= event.pos[0] <= 825 and 625 <= event.pos[1] <= 719:
 					carte2 = tirage_de_la_carte()
 			
-					fenetre.blit(carte2, (585,435))
+					fenetre.blit(carte2, (505,435))
 
 			pygame.display.flip()
 	
