a095e47d9497a63c68d98619b1488c8743050f9a
LaymajouxClement <laymajouxclement@gmail.com>
2019-01-12 09:57:55 +0100

    rajout du croupier

--- main_fenetre.py
+++ main_fenetre.py
@@ -2,42 +2,45 @@ import pygame
 from pygame.locals import *
 
 def main():
-	from tirage_carte import tirage_de_la_carte 
-	from tirer import bouton_tirer
-	from windows import windows
-	
+    from tirage_carte import tirage_de_la_carte
+    from tirer import bouton_tirer
+    from windows import windows
 
-	pygame.init()
 
-	fenetre = windows()
-	
-	continuer = 1
+    pygame.init()
 
-	
+    fenetre = windows()
 
-	table = pygame.image.load("blackjack_table.png").convert()
-	fenetre.blit(table, (0,0))
+    continuer = 1
 
-	carte = tirage_de_la_carte()
-	fenetre.blit(carte, (485,435))
 
-	boutonTirage = bouton_tirer()
-	fenetre.blit(boutonTirage, (650,625))
 
-	while continuer:
-		for event in pygame.event.get():
-			if event.type == QUIT:    
-				continuer = 0
-			if event.type == MOUSEBUTTONDOWN:
-				if event.button == 1 and 650<= event.pos[0] <= 825 and 625 <= event.pos[1] <= 719:
-					carte2 = tirage_de_la_carte()
-			
-					fenetre.blit(carte2, (505,435))
+    table = pygame.image.load("blackjack_table.png").convert()
+    fenetre.blit(table, (0,0))
 
-			pygame.display.flip()
-	
+    carte = tirage_de_la_carte()
+    fenetre.blit(carte, (485,435))
 
-		pygame.display.flip()
+    croupier = pygame.image.load("croupier.png").convert()
+    fenetre.blit(croupier, (485, 0))
 
-	
+    boutonTirage = bouton_tirer()
+    fenetre.blit(boutonTirage, (650,625))
+
+    while continuer:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                continuer = 0
+       	    if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1 and 650<= event.pos[0] <= 825 and 625 <= event.pos[1] <= 719:
+                    carte2 = tirage_de_la_carte()
+
+                    fenetre.blit(carte2, (505,435))
+
+            pygame.display.flip()
+
+
+        pygame.display.flip()
+
+
 main()
