459fac76479d8e6e9d70255d09e165732a2e7879
matthieurobert <matthieurobert82@gmail.com>
2019-01-24 08:35:48 +0100

    ajout de com

--- main_fenetre.py
+++ main_fenetre.py
@@ -11,8 +11,8 @@ def main():
 
 
 
-    somme=0                                                                     # la valeur des cartes
-    croupier= 0
+    somme=0                                # la valeur des cartes du joueur
+    croupier= 0                            # la valeur des cartes du croupier
 
     fenetre = windows()                                                          # on créer une fenêtre
 
@@ -25,23 +25,23 @@ def main():
 
     carte = tirage_de_la_carte()                                                 # on récupère la carte du programme tirage_de_la_carte
     fenetre.blit(carte[0], (485,435))                                               # dont la résultante et l'image obtenue Ex: 1♣.png
-    somme = somme + carte[1]
+    somme = somme + carte[1]                                                    # On actualise la valeur des cartes du joueur
     print(somme)
 
-    pos_player = 525
-    pos_croup = 485
+    pos_player = 525                                                            # On créer une variable pour la position initiale des cartes du joueur 
+    pos_croup = 485                                                             # On créer une variable pour la position initiale des cartes du croupier 
 
 
     carte = tirage_de_la_carte()                                                 # on refait de même mais avec une autre carte
     fenetre.blit(carte[0], (505,435))                                               # on la décalle de 20 pixels par rapport à l'autre carte
-    somme = somme + carte[1]
+    somme = somme + carte[1]                                                    # On actualise la valeur des cartes du joueur
     print(somme)
 
     boutonTirage = bouton_tirer()                                                #on affiche le bouton tirer
-    fenetre.blit(boutonTirage, (875,625))                                       #on l'affiche aux coordonnées 650,625
+    fenetre.blit(boutonTirage, (875,625))                                       #on l'affiche aux coordonnées 875,625
 
-    boutonSTOP = bouton_stop()
-    fenetre.blit(boutonSTOP,(90,625))
+    boutonSTOP = bouton_stop()                                                  # On affiche le bouton stop
+    fenetre.blit(boutonSTOP,(90,625))                                           # On l'affiche aux coordonnées 90,625
 
     while continuer:                                                             # boucle qui permet de garder la fenêtre ouverte
         for event in pygame.event.get():                                         # si il y a un événement de type "CLICK" faire:
@@ -50,22 +50,22 @@ def main():
        	    if event.type == MOUSEBUTTONDOWN:                                    # si l'on clique avec la souris:
                 if event.button == 1 and 875<= event.pos[0] <= 1050 and 625 <= event.pos[1] <= 719: # si le clic est dans cette zone ( TIRER ) :
                     carte2 = tirage_de_la_carte()                                # importe une carte
-                    somme = somme + carte2[1]
+                    somme = somme + carte2[1]                                     # On actualise la valeur des cartes du joueur
                     
                     print(somme)
                     fenetre.blit(carte2[0], (pos_player,435))                             # affiche la carte
-                    pos_player = pos_player + 20
+                    pos_player = pos_player + 20                                # On décale la carte à afficher de 20 pixels
                     pygame.display.flip()
 
                     if somme > 21 :
                     	perdu = PERDU()
-                    	fenetre.blit(perdu,(250,215))
-                    	continuer = 0
+                    	fenetre.blit(perdu,(250,215))                         # On affiche l'image perdu.png
+                    	                                         
 
-                if event.button == 1 and 90<= event.pos[0] <= 575 and 625 <= event.pos[1] <= 719:
+                if event.button == 1 and 90<= event.pos[0] <= 575 and 625 <= event.pos[1] <= 719:# Si on clique sur le bouton ( STOP )
                 	if croupier == 0 :
                 		carte_croup1 =tirage_de_la_carte()
-                		croupier = croupier + carte_croup1[1]
+                		croupier = croupier + carte_croup1[1]   
                 		print("c o croupier")
                 		print(croupier)
 
@@ -97,24 +97,22 @@ def main():
 
                 			pygame.time.wait(2000)
 
-        pygame.time.wait(5000)
+                		if croupier >= 1 :
 
-        if croupier >= 1:
+                                    if (somme > 21 and somme < croupier) or croupier == 21:
+                                        perdu = PERDU()
+                                        fenetre.blit(perdu,(250,215))
+                
+                                        
 
-        	if (somme > 21 and somme < croupier) or croupier == 21:
-        		perdu = PERDU()
-        		fenetre.blit(perdu,(250,215))
-        		pygame;time.wait(5000)
-        		continuer = 0
+                                    elif somme >= croupier or croupier > 21:
+                                        gagne = GAGNE()
+                                        fenetre.blit(gagne,(250,215))
+                                        pygame.time.wait(5000)
+                                        
 
-        	elif somme > croupier or croupier > 21:
-        		gagne = GAGNE()
-        		fenetre.blit(gagne,(250,215))
-        		pygame.time.wait(5000)
-        		continuer = 0
-
-        	pygame.display.flip()                                                # raffraichi la fenêtre
-        pygame.display.flip()                                                    # raffraichi la fenêtre
+            pygame.display.flip()        # raffraichi la fenêtre
+        pygame.display.flip()             # raffraichi la fenêtre
 
 
 
--- perdu_gagné.py
+++ perdu_gagné.py
@@ -1,20 +1,20 @@
-from pygame.locals import *
+from pygame.locals import *   # On importe pygame
 import pygame
 
-def PERDU():
+def PERDU():  # Création de la fonction perdu
 
-    pygame.init()
+    pygame.init() # initialisation de pygame
 
-    perdu = pygame.image.load("perdu.png").convert_alpha()
+    perdu = pygame.image.load("perdu.png").convert_alpha()  # On charge l'image perdu.png et on la stocke dans la variable perdu
 
-    return perdu
+    return PERDU # On renvoie la variable perdu
 
 
 
-def GAGNE():
+def GAGNE():  # Création de la fonction gagné
 
-    pygame.init()
+    pygame.init()  # On initialise pygame
 
-    gagne = pygame.image.load("gagné.png").convert_alpha()
+    gagne = pygame.image.load("gagné.png").convert_alpha() # On charge l'image gagné.png et on la stoke dans la variable gagne
 
-    return(gagne)
+    return(gagne)  # On renvoie la variable gagne
