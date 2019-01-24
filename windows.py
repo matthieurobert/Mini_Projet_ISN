3d193a2e82155f935612342653d3fd104830af95
matthieurobert <matthieurobert82@gmail.com>
2019-01-24 09:33:57 +0100

    a

--- windows.py
+++ windows.py
@@ -1,9 +1,9 @@
-import pygame
+import pygame						# On importe pygame
 from pygame.locals import *
 
-def windows():
-	pygame.init()
+def windows():						# On créer la fonction windows
+	pygame.init()					# On initialise pygame
 
-	fenetre = pygame.display.set_mode((1145,720))
+	fenetre = pygame.display.set_mode((1145,720))    # On créer la fenêtre et on la stocke dans une variable
 	
-	return fenetre
+	return fenetre                  # On renvoie la variable fenetre
